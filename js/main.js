b504d99885c4f5dd6b992167ae8264dded6612ec
Felipe Jorge <felipejorgecd@gmail.com>
2022-04-04 18:13:55 -0300

    adicionando o form event

--- js/main.js
+++ js/main.js
@@ -1,34 +1,51 @@
-var paciente = document.querySelector ('.primeiroPaciente');
+var pacientes = document.querySelectorAll(".paciente");
 
+for (var i = 0; i < pacientes.length; i++) {
 
-var tdPeso = document.querySelector ('.info-peso');
-var peso = tdPeso.textContent;
+    var paciente = pacientes[i];
 
-var tdAltura = document.querySelector ('.info-altura');
-var altura = tdAltura.textContent;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
+    var tdImc = paciente.querySelector(".info-imc");
 
-var tdImc = paciente.querySelector('.info-imc');
+    var pesoValido = true;
+    var alturaValida = true;
 
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoValido = false;
+        tdImc.textContent = "Peso Inválido";
+        paciente.classList.add("paciente-invalido");
+        
+    }
 
-var pesoValido = true;
-var alturaValida = true;
+    if (altura <= 0 || altura >= 3.00) {
+        
+        console.log("Altura inválida!");
+        alturaValida = false;
+        tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+        
+    }
 
-
-
-if (peso <= 0 || peso >= 1000){
-    console.log ("Peso inválido");
-    pesoValido = false;
-    tdImc.textContent = "Peso Inválido!";
+  if (alturaValida && pesoValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
-if (altura <= 0 || altura >= 3.00){
-    console.log ("Altura inválida")
-    alturaValida = false;
-    tdImc.textContent = "Altura Inválida!";
-}
-if (pesoValido && alturaValida){
-    const imc = peso / (altura*altura);
-    tdImc.textContent = imc;
+// eventos 
 
-}
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener ("click", function(event){
+    event.preventDefault();
+    console.log("botao clicado");
+});
+
+    
+
+
+
